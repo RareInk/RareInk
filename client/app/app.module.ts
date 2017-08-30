dfced39774cfe104ec87a74255465471cd445cc3
Resi Respati <resi.respati@c88fin.com>
2017-08-30 12:52:21 +0700

    added Home component
    [Tentative] We'll probably use this for the initial homepage. Unless we're going straight-to-app like Discord does.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { StoreModule } from '@ngrx/store';
@@ -7,6 +8,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { routes } from './app.router';
 import { metaReducers, reducers } from './store';
 import { SharedModule } from './shared/shared.module';
@@ -15,11 +17,11 @@ import { WeatherEffects } from './store/weather/weather.effects';
 import { FeedEffects } from './store/feed/feed.effects';
 import { ProfileEffects } from './store/profile/profile.effects';
 import { environment } from '../environments/environment';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- client/app/app.router.ts
+++ client/app/app.router.ts
@@ -1,7 +1,8 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Route[] = [
-  { path: '', pathMatch: 'full', redirectTo: 'weather'},
+  { path: '', pathMatch: 'full', component: HomeComponent},
   { loadChildren: 'app/dashboard/dashboard.module#DashboardModule', path: 'dashboard' },
   { loadChildren: 'app/profile/profile.module#ProfileModule', path: 'profile' },
   { loadChildren: 'app/weather/weather.module#WeatherModule', path: 'weather' }
