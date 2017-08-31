793cafc94ce50d0fc923bb3b56e89c1ec02d88a2
Resi Respati <resir014@gmail.com>
2017-08-31 08:01:28 +0700

    Begin switch to Sequelize

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,5 +1,33 @@
-import { Schema, model } from "mongoose";
-import { IUser } from "../interfaces/user";
+import * as Sequelize from 'sequelize'
 
-export interface IUserModel extends IUser, Document {
+export interface LanguageAttributes {
+  id?: string
+  label?: string
+  name?: string
+}
+
+export interface LanguageInstance extends Sequelize.Instance<LanguageAttributes> {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+
+  label: string
+  name: string
+}
+
+export default function defineUser(sequelize: Sequelize.Sequelize, DataTypes) {
+  const Language = sequelize.define('Language', {
+    label: DataTypes.STRING(255),
+    name: DataTypes.STRING(50)
+  }, {
+      classMethods: {
+        associate: function(models) {
+          Language.hasMany(models.AppUser, {
+            foreignKey: 'languageId',
+            as: 'appUsers'
+          })
+        }
+      }
+    })
+  return Language
 }
